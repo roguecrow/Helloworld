f3aef24122326693315879bbf4836033d29a86da
baburajkumar <baburajkumar2001@psnacet.edu.in>
2023-03-29 17:43:43 +0530

    added popup

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,74 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( const MaterialApp(
+    home:  MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  const MyApp({super.key});
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<StatefulWidget> createState() => MyAppState();
 }
-
-class _MyAppState extends State<MyApp> {
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Center(child:Text('HeLLoWoRLd')),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('" HELLO WORLD "'),
+      ),
+      body: Container(
+        child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('WELCOME'),
+                ElevatedButton(
+                  child: Text('Click For Next Page'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Nextpage()),
+                    );
+                    showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text('ALERT'),
+                          content: Text('You are redirected to next page'),
+                          actions: [
+                            TextButton(onPressed: ()=> Navigator.pop(context),
+                                child: Text('OK'),)
+                          ],
+                        ),
+                    );
+                  },
+                ),
+              ],
+            ),
+        ),
+      ),
     );
   }
 }
+
+class Nextpage extends StatelessWidget {
+  const Nextpage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('hello there'),
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed: () {
+          Navigator.pop(context);
+          },
+            child: Text('TO 1st page')
+        ),
+      ),
+    );
+  }
+}
+
